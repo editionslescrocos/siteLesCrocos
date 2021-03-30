33017bfd4dbbbaa83788a28ecb60ae54216a9f8a
Fabien <fab.grignoux@gmail.com>
2021-03-30 12:04:17 +0200

    update sitemap to exclude certains url

--- utils/getRoutes.js
+++ utils/getRoutes.js
@@ -2,5 +2,23 @@ export default async () => {
   const { $content } = require("@nuxt/content");
   const files = await $content({ deep: true }).only(["path"]).fetch();
 
-  return files.map((file) => (file.path === "/index" ? "/" : file.path));
+  const forbidenWords=["team","slider","press","general"]
+
+  const paths =  files.map((file) => (file.path === "/index" ? "/" : file.path));
+
+  const isWordPresent=function(path){
+    let isWordPresent=false
+    forbidenWords.forEach(
+      word=>{
+        if(path.includes(word))isWordPresent=true
+      }
+    )
+
+    return isWordPresent
+  }
+
+  console.log("chemins: ",paths.filter(path=>!isWordPresent(path)));
+
+  return paths.filter(path=>!isWordPresent(path))
+
 };
