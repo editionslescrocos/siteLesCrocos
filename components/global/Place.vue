77163ab10bd29a5f01add8159b186870289b2683
Fabien <fab.grignoux@gmail.com>
2021-06-23 11:20:36 +0200

    Auto stash before merge of "main" and "origin/main"

--- components/global/Place.vue
+++ components/global/Place.vue
@@ -31,8 +31,22 @@
           />
           Site web</Btn
         >
-      </a>
-    </p>
+      </p>
+      <p>
+        <a v-if="website" :href="website" target="blank">
+          <Btn
+            ><img
+              :src="require('@/assets/chain.svg')"
+              alt=""
+              height="20"
+              width="20"
+              class="inline-block w-5 h-5 mr-1"
+            />
+            Site web</Btn
+          >
+        </a>
+      </p>
+    </div>
   </div>
 </template>
 
