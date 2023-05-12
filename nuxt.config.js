d77b244325e689449229393952470e8a6773a0ab
Fabien Grignoux <fabien.grignoux@outlook.com>
2023-05-12 16:06:55 +0200

    fix MDX import

--- nuxt.config.js
+++ nuxt.config.js
@@ -74,7 +74,6 @@ export default {
     "@nuxtjs/snipcart",
     "nuxt-compress",
     "@aceforth/nuxt-optimized-images",
-    "@nuxtjs/mdx",
     "@nuxt/image",
     [
       "@nuxtjs/google-fonts",
